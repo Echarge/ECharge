0fe0c87ca20a8a1372fdd47feb54d494db58196f
Siva Mekala <siva@Sivas-MacBook-Air.local>
2024-11-21 23:25:49 +0530

    One

---
+++ EChargeTests/EChargeTests.swift
@@ -0,0 +1,17 @@
+//
+//  EChargeTests.swift
+//  EChargeTests
+//
+//  Created by Siva Mekala on 21/11/24.
+//
+
+import Testing
+@testable import ECharge
+
+struct EChargeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
